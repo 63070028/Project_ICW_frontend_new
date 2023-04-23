82ec1f21379bbba77d13cd58e23d36dacef4063c
63070107 <73679556+panascharoenruk@users.noreply.github.com>
2023-04-23 17:40:18 +0700

    potter: commit

--- src/components/company-list-applicant-jobs.vue
+++ src/components/company-list-applicant-jobs.vue
@@ -1,165 +1,125 @@
 <template>
-  <div class="column m-0 p-0">
-    <div class="card px-5 py-3">
-      <div class="pt-3" style="border-top: 0.5px solid gray" v-for="(item, index) in states.addItemsPageList"
-        :key="index">
-        <div @click="viewApplicant(item.id)">
-          <p class="is-size-4 has-text-weight-bold p-4">
-            id: {{ item.id }}. {{ item.firstName }} {{ item.lastName }}
-          </p>
-          <div class="columns p-4">
-            <div class="column">
-              <p class="is-size-5 has-text-weight-bold">
-                ตำแหน่ง: {{ item.job_id }}
-              </p>
-            </div>
-            <div class="column">
-              <p class="is-size-5 has-text-weight-bold">สถานะ: {{ item.state }}</p>
-            </div>
-            <div class="column">
+    <div class="column m-0 p-0">
+        <div class="card px-5 py-3">
+            <div class="pt-3" style="border-top:0.5px solid gray;" v-for="item, index in paginatedItems" :key="index">
 
+                <p class="is-size-4 has-text-weight-bold p-4">ตำแหน่ง {{ item.job_name }}</p>
+                <div class="columns p-4" @click="viewApplicationDetail(item.applicant_id)">
+                    <div class="column">
+                        <p class="is-size-5 has-text-weight-bold">ตำแหน่ง: {{ item.job_name }}</p>
+                    </div>
+                    <div class="column">
+                        <p class="is-size-5 has-text-weight-bold">วันที่ยื่นสมัคร: {{ item.creation_date }}</p>
+                    </div>
+                    <div class="column">
+                        <p class="is-size-5 has-text-weight-bold">สถานะ: {{ item.state }}</p>
+                    </div>
+                </div>
+                <div style="display: flex; flex-direction: row; justify-content: flex-end;">
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
 
-    <nav class="pagination mt-3 pl-6 pr-5" role="navigation" aria-label="pagination">
-      <button class="pagination-previous" :disabled="previousClicked" @click="getPreviousPage()">
-        Previous
-      </button>
-      <button class="pagination-next" :disabled="nextPageClicked" @click="getNextPage()">
-        Next page
-      </button>
-      <ul class="pagination-list">
-        <li v-for="index in states.countOfPages" :key="index">
-          <a v-bind:id="'pageId' + (index + 1)" class="pagination-link" @click="changePage(index + 1)">{{ index + 1 }}</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+        <nav class="pagination mt-3 pl-6 pr-5" role="navigation" aria-label="pagination">
+            <button class="pagination-previous" :disabled="previousClicked" @click="getPreviousPage()">Previous</button>
+            <button class="pagination-next" :disabled="nextPageClicked" @click="getNextPage()">Next page</button>
+            <ul class="pagination-list">
+                <li v-for="n in countOfPages" :key="n">
+                    <a v-bind:id="'pageId' + (n)" class="pagination-link" @click="changePage(n)">{{ n }}</a>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  onMounted,
-  onUpdated,
-  reactive,
-  ref,
-  PropType,
-} from "vue";
-import ApplicationJob from "@/models/ApplicationJob";
-import { useRouter } from "vue-router";
+import Swal from 'sweetalert2';
+import { computed, ComputedRef, defineComponent, onMounted, onUpdated, PropType, ref } from 'vue'
+import ApplicationJob from '@/models/ApplicationJob';
+import axios from '@/plugins/axios';
+import { PORT } from '@/port';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
-  props: {
-    items: {
-      type: Object as PropType<ApplicationJob[]>,
-      required: true,
+    props: {
+        items: {
+            type: Object as PropType<ApplicationJob[]>,
+            required: true,
+        },
+        itemPerEachPage: {
+            type: Number,
+            default: 3
+        },
+        isCancel: {
+            type: Boolean,
+            default: true
+        },
     },
-    itemPerEachPage: {
-      type: Number,
-      default: 3,
+    setup(props, { emit }) {
+        const router = useRouter();
+        let presentPage = ref<number>(1);
+        let pastPage = ref<number>(1);
+
+        const nextPageClicked = ref<boolean>(false);
+        const previousClicked = ref<boolean>(false);
+
+
+
+        onMounted(() => {
+            validatePageCount();
+        })
+
+        onUpdated(() => {
+            document.getElementById('pageId' + presentPage.value)?.classList.add('is-current');
+        })
+
+        const getNextPage = () => {
+            pastPage.value = presentPage.value;
+            presentPage.value += 1;
+            validatePageCount()
+            document.getElementById('pageId' + presentPage.value)?.classList.add('is-current');
+            document.getElementById('pageId' + pastPage.value)?.classList.remove('is-current');
+        }
+
+        const changePage = (page: number) => {
+            pastPage.value = presentPage.value;
+            presentPage.value = page;
+            validatePageCount()
+            document.getElementById('pageId' + presentPage.value)?.classList.add('is-current');
+            document.getElementById('pageId' + pastPage.value)?.classList.remove('is-current');
+        }
+
+        const getPreviousPage = () => {
+            pastPage.value = presentPage.value;
+            presentPage.value -= 1;
+            validatePageCount()
+            document.getElementById('pageId' + presentPage.value)?.classList.add('is-current');
+            document.getElementById('pageId' + pastPage.value)?.classList.remove('is-current');
+        }
+
+
+        const paginatedItems = computed(() => {
+            let startItem = (presentPage.value - 1) * props.itemPerEachPage;
+            let endItem = startItem + props.itemPerEachPage;
+            return props.items.slice(startItem, endItem);
+        })
+
+        const countOfPages: ComputedRef<number> = computed(() => {
+            return Math.ceil(props.items.length / props.itemPerEachPage)
+        })
+
+        const validatePageCount = () => {
+            presentPage.value === countOfPages.value ? nextPageClicked.value = true : nextPageClicked.value = false;
+            presentPage.value === 1 ? previousClicked.value = true : previousClicked.value = false;
+        }
+        const viewApplicationDetail = (id: string) => {
+            router.push("/Applicant/" + id)
+        }
+
+        return {
+            nextPageClicked, previousClicked, getNextPage, getPreviousPage, presentPage, changePage, paginatedItems, countOfPages, viewApplicationDetail
+        }
     },
-    isCancel: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  setup(props) {
-    const router = useRouter();
-    let presentPage = ref<number>(1);
-    let pastPage = ref<number>(1);
-    const states = reactive<{
-      countOfPages: number[];
-      addItemsPageList: ApplicationJob[];
-    }>({
-      countOfPages: [],
-      addItemsPageList: [],
-    });
-
-    const nextPageClicked = ref<boolean>(false);
-    const previousClicked = ref<boolean>(false);
-
-    onMounted(() => {
-      loadMyPaginationList();
-      states.countOfPages = Array.from(
-        Array(Math.ceil(props.items.length / props.itemPerEachPage)).keys()
-      );
-    });
-
-    onUpdated(() => {
-      loadMyPaginationList();
-      document
-        .getElementById("pageId" + presentPage.value)
-        ?.classList.add("is-current");
-    });
-
-    const getNextPage = () => {
-      pastPage.value = presentPage.value;
-      presentPage.value += 1;
-      loadMyPaginationList();
-      document
-        .getElementById("pageId" + presentPage.value)
-        ?.classList.add("is-current");
-      document
-        .getElementById("pageId" + pastPage.value)
-        ?.classList.remove("is-current");
-    };
-
-    const changePage = (page: number) => {
-      pastPage.value = presentPage.value;
-      presentPage.value = page;
-      loadMyPaginationList();
-      document
-        .getElementById("pageId" + presentPage.value)
-        ?.classList.add("is-current");
-      document
-        .getElementById("pageId" + pastPage.value)
-        ?.classList.remove("is-current");
-    };
-
-    const getPreviousPage = () => {
-      pastPage.value = presentPage.value;
-      presentPage.value -= 1;
-      loadMyPaginationList();
-      document
-        .getElementById("pageId" + presentPage.value)
-        ?.classList.add("is-current");
-      document
-        .getElementById("pageId" + pastPage.value)
-        ?.classList.remove("is-current");
-    };
-
-    const loadMyPaginationList = () => {
-      let startItem = (presentPage.value - 1) * props.itemPerEachPage;
-      let endItem = startItem + props.itemPerEachPage;
-      states.addItemsPageList = props.items.slice(startItem, endItem);
-      validatePageCount();
-    };
-
-    const validatePageCount = () => {
-      presentPage.value === states.countOfPages.length
-        ? (nextPageClicked.value = true)
-        : (nextPageClicked.value = false);
-      presentPage.value === 1
-        ? (previousClicked.value = true)
-        : (previousClicked.value = false);
-    };
-    const viewApplicant = (id: string) => {
-      router.push("/applicant/" + id)
-    }
-    return {
-      nextPageClicked,
-      previousClicked,
-      getNextPage,
-      getPreviousPage,
-      states,
-      presentPage,
-      changePage,
-      viewApplicant
-    };
-  },
-});
+})
 </script>
--- src/components/company-list-applicant.vue
+++ src/components/company-list-applicant.vue
@@ -1,109 +1,68 @@
 <template>
-  <div class="column ml-6 p-0">
-    <div class="tabs">
-      <ul>
-        <li
-          :class="[select_option_display === 'jobs' ? 'is-active' : '']"
-          @click="select_option_display = 'jobs'"
-        >
-          <a>งาน</a>
-        </li>
-        <li
-          :class="[select_option_display === 'programs' ? 'is-active' : '']"
-          @click="select_option_display = 'programs'"
-        >
-          <a>โครงการ</a>
-        </li>
-      </ul>
+    <div class="column ml-6 p-0">
+
+        <div class="tabs">
+            <ul>
+                <li :class="[select_option_display === 'jobs' ? 'is-active' : '']" @click="select_option_display = 'jobs'">
+                    <a>งาน</a>
+                </li>
+                <li :class="[select_option_display === 'programs' ? 'is-active' : '']"
+                    @click="select_option_display = 'programs'"><a>โครงการ</a></li>
+            </ul>
+        </div>
+
+        <ApplicationsJobList v-if="select_option_display === 'jobs'" :items="myApplications.applicationJob" @cancelApplicationJob="updateMyApplicationJob($event)"
+            :isCancel="enableCancel">
+        </ApplicationsJobList>
+        <myjobApplicationProgramList v-if="select_option_display === 'programs'" :items="myApplications.applicationProgram" @cancelApplicationProgram="updateMyApplicationProgram($event)"
+            :isCancel="enableCancel">
+        </myjobApplicationProgramList>
     </div>
-
-    <listApplicantJobs
-      v-if="select_option_display === 'jobs'"
-      :items="applicationJobs"
-      :isCancel="enableCancel"
-    >
-    </listApplicantJobs>
-
-    <listApplicantProgram
-      v-if="select_option_display === 'programs'"
-      :items="applicationPrograms"
-      :isCancel="enableCancel"
-    >
-    </listApplicantProgram>
-  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, onMounted, reactive } from "vue";
-import Applications from "@/models/Applications";
-import ApplicationJob from "@/models/ApplicationJob";
-import ApplicationProgram from "@/models/ApplicationProgram";
-import listApplicantJobs from "./company-list-applicant-jobs.vue";
-import listApplicantProgram from "./company-list-applicant-program.vue";
-import axios from "axios";
-import { PORT } from "@/port";
+import { defineComponent, PropType, ref } from 'vue'
+import Applications from '@/models/Applications';
+import ApplicationsJobList from './company-list-applicant-jobs.vue';
+import myjobApplicationProgramList from './company-list-applicant-program.vue';
 
 export default defineComponent({
-  props: {
-    myApplications: {
-      type: Object as PropType<Applications>,
-      default: () => {
+    emits:["cancelApplicationJob", "cancelApplicationProgram"],
+    props: {
+        myApplications: {
+            type: Object as PropType<Applications>,
+            default: () => {
+                return {
+                    applicationJob: [],
+                    applicationProgram: []
+                }
+            }
+        },
+        enableCancel: {
+            type: Boolean,
+            required: true
+        }
+    },
+    components: {
+        ApplicationsJobList,
+        myjobApplicationProgramList
+    },
+    setup(props, {emit}) {
+
+        const select_option_display = ref<string>('jobs');
+
+        
+        const updateMyApplicationJob = (id:string) => {
+            emit("cancelApplicationJob", id)
+        }
+
+        const updateMyApplicationProgram = (id:string) => {
+            emit("cancelApplicationProgram", id)
+        }
+
         return {
-          applicationJob: [],
-          applicationProgram: [],
-        };
-      },
+            select_option_display, updateMyApplicationJob, updateMyApplicationProgram
+        }
     },
-    enableCancel: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  components: {
-    listApplicantJobs,
-    listApplicantProgram,
-  },
-  setup() {
-    
-    const select_option_display = ref<string>("jobs");
-    const applicationJobs = reactive<ApplicationJob[]>([]);
-    const applicationPrograms = reactive<ApplicationProgram[]>([]);
-    onMounted(() => {
-  
-      axios
-        .get(`${PORT}` + "/application/getApplicantJob")
-        .then((response) => {
-          console.log(response.data.items);
-          const get_applicationJob: ApplicationJob[] = response.data.items;
-          console.log(get_applicationJob);
-          get_applicationJob.forEach((appliacnt) => {
-            applicationJobs.push(appliacnt);
-          });
-        })
-        .catch((error) => {
-          // handle error
-          console.log(error);
-        }); 
-        axios
-        .get(`${PORT}` + "/application/getApplicantProgram")
-        .then((response) => {
-          console.log(response.data.items);
-          const get_applicationProgram: ApplicationProgram[] = response.data.items;
-          console.log(get_applicationProgram);
-          get_applicationProgram.forEach((appliacnt) => {
-            applicationPrograms.push(appliacnt);
-          });
-        })
-        .catch((error) => {
-          // handle error
-          console.log(error);
-        });
-    });
-
-
-    return {
-      select_option_display, applicationJobs, applicationPrograms
-    };
-  },
-});
+})
 </script>
