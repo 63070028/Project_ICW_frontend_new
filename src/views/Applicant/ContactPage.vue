807671bb62901398803e6ebef7a00c10c75dc510
63070028 <63070028@kmitl.ac.th>
2023-04-27 17:27:16 +0700

    Update ContactPage.vue

--- src/views/Applicant/ContactPage.vue
+++ src/views/Applicant/ContactPage.vue
@@ -1,55 +1,109 @@
 <template>
-    <div class="container card p-6 mt-6">
-        <p class="is-size-1 has-text-centered ">รายชื่ออาจารย์</p>
-        <div class="columns is-multiline mt-6">
-            <div class="card column is-3 m-6 has-background-grey-lighter p-0">
-                <img class="image_profile"
-                    src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Boonprasert-768x768.jpg">
-                <div class="px-3">
-                    <p class="is-size-5 has-text-weight-semibold">ผศ.ดร. บุญประเสริฐ สุรักษ์รัตนสกุล
-                    </p>
-                    <p class="is-size-5 has-text-weight-medium">boonprasert@it.kmitl.ac.th</p>
-                    <p class="is-size-5 has-text-weight-medium">Office: 522</p>
-                </div>
-            </div>
-            <div class="card column is-3 m-6 has-background-grey-lighter p-0">
-                <img class="image_profile"
-                    src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Pattanapong-768x768.jpg">
-                <div class="px-3">
-                    <p class="is-size-5 has-text-weight-semibold">ดร. พัฒนพงษ์ ฉันทมิตรโอภาส</p>
-                    <p class="is-size-5 has-text-weight-medium">pattanapong@it.kmitl.ac.th</p>
-                    <p class="is-size-5 has-text-weight-medium">Office: 610</p>
-                </div>
-            </div>
-            <div class="card column is-3 m-6 has-background-grey-lighter p-0">
-                <img class="image_profile" src="https://www.it.kmitl.ac.th/wp-content/uploads/2018/03/Lapas-768x768.jpg">
-                <div class="px-3">
-                    <p class="is-size-5 has-text-weight-semibold">ผศ.ดร. ลภัส ประดิษฐ์ทัศนีย์</p>
-                    <p class="is-size-5 has-text-weight-medium">lapas@it.kmitl.ac.th</p>
-                    <p class="is-size-5 has-text-weight-medium">Office: 525</p>
-                </div>
-            </div>
+  <div class="container card p-6 mt-6">
+    <p class="is-size-1 has-text-centered">รายชื่ออาจารย์</p>
+    <div class="columns is-multiline mt-6">
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Boonprasert-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">
+            ผศ.ดร. บุญประเสริฐ สุรักษ์รัตนสกุล
+          </p>
+          <p class="is-size-5 has-text-weight-medium">
+            boonprasert@it.kmitl.ac.th
+          </p>
+          <p class="is-size-5 has-text-weight-medium">Office: 522</p>
         </div>
+      </div>
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Pattanapong-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">
+            ดร. พัฒนพงษ์ ฉันทมิตรโอภาส
+          </p>
+          <p class="is-size-5 has-text-weight-medium">
+            pattanapong@it.kmitl.ac.th
+          </p>
+          <p class="is-size-5 has-text-weight-medium">Office: 610</p>
+        </div>
+      </div>
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2018/03/Lapas-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">
+            ผศ.ดร. ลภัส ประดิษฐ์ทัศนีย์
+          </p>
+          <p class="is-size-5 has-text-weight-medium">lapas@it.kmitl.ac.th</p>
+          <p class="is-size-5 has-text-weight-medium">Office: 525</p>
+        </div>
+      </div>
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Arit-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">รศ.ดร. อาริต ธรรมโน</p>
+          <p class="is-size-5 has-text-weight-medium">arit@it.kmitl.ac.th</p>
+          <p class="is-size-5 has-text-weight-medium">Office: 626</p>
+        </div>
+      </div>
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2017/12/Chotipat-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">
+            รศ.ดร. โชติพัชร์ ภรณวลัย
+          </p>
+          <p class="is-size-5 has-text-weight-medium">
+            chotipat@it.kmitl.ac.th
+          </p>
+          <p class="is-size-5 has-text-weight-medium">Office: 624</p>
+        </div>
+      </div>
+      <div class="card column is-3 m-6 has-background-grey-lighter p-0">
+        <img
+          class="image_profile"
+          src="https://www.it.kmitl.ac.th/wp-content/uploads/2018/03/Teerapong-768x768.jpg"
+        />
+        <div class="px-3">
+          <p class="is-size-5 has-text-weight-semibold">
+            รศ.ดร. ธีรพงศ์ ลีลานุภาพ
+          </p>
+          <p class="is-size-5 has-text-weight-medium">
+            teerapong@it.kmitl.ac.th
+          </p>
+          <p class="is-size-5 has-text-weight-medium">Office: 510-1</p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    setup() {
-
-        return {
-
-        }
-    },
-})
+  setup() {
+    return {};
+  },
+});
 </script>
 
 <style scoped>
 .image_profile {
-    height: 320px;
+  height: 320px;
 }
 </style>
