b5eb7bfe31d6c939b1b4e45745e65da1969b275f
63070028 <63070028@kmitl.ac.th>
2023-04-27 17:41:13 +0700

    Update port.ts

--- src/port.ts
+++ src/port.ts
@@ -1,2 +1,2 @@
-export const PORT = "http://localhost:5000";
-// export const PORT = "http://icw-LB-1253960079.us-east-1.elb.amazonaws.com";
+// export const PORT = "http://localhost:5000";
+export const PORT = "http://icw-LB-1253960079.us-east-1.elb.amazonaws.com";
